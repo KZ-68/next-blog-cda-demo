2ec12a9a5ade5af077d56588b9ed672c67c4f089
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-09-06 17:28:30 +0200

    feat: add layout header for nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <header>
+        <NavBar />
+      </header>
         <main className="bg-slate-800 min-h-screen p-6 text-white">
           {children}
         </main>
