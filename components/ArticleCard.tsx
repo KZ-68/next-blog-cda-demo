d52064c4e1bb4aa3b5203ba9d1839224d4df15df
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-09-06 17:26:28 +0200

    feat: add props

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { formatDate } from '@/lib/utils'
 import Button from './Button'
+import Tag from './Tag'
 
-const ArticleCard:React.FC<any> = ({article}) => {
+interface ArticleCardProps {
+    article: ArticleWithTagsAndComments;
+}
+
+const ArticleCard:React.FC<ArticleCardProps> = ({article}) => {
     return (
         <div className='group border border-slate-500 p-6 rounded-md hover:bg-slate-700 cursor-pointer hover:translate-y-2 duration-300' key={article.id}>
             {/* Titre de l'article */}
@@ -10,9 +15,7 @@ const ArticleCard:React.FC<any> = ({article}) => {
             <p className='text-sm text-slate-300'>{formatDate(article.createdAt)} {formatDate(article.createdAt)}</p>
             <div className='flex flex-wrap gap-2 my-4'>
                 {article.tags.map((tagArticle:any)=> (
-                    <span className='group: px-3 py-2 text-xs rounded-full bg-slate-600 group-hover:bg-pink-600 duration-500' key={tagArticle.tag.id}>
-                        {tagArticle.tag.name}
-                    </span>
+                    <Tag text={tagArticle.tag.name} key={tagArticle.tag.id}/>
                 ))}
             </div>
             
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Button:React.FC<any> = ({label, href}) => {
+interface ButtonProps {
+  label: string;
+  href: string;
+}
+
+const Button:React.FC<ButtonProps> = ({label, href}) => {
   return (
     <Link className='inline-block px-5 py-2 mt-3 bg-cyan-900 hover:bg-cyan-800 rounded-lg' href={href}>
       {label}
